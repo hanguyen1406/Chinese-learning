9a0d3d01e16462175684147e39f484227dd107d9
hanguyen1406 <hanguyen1406@gmail.com>
2025-10-20 19:04:22 +0700

    upload

---
+++ src/app/model/type-of-teaching.ts
@@ -0,0 +1,10 @@
+export interface TypeOfTeaching {
+    id: number,
+    naziv: Type;
+}
+
+export enum Type{
+    LECTURES,
+	EXERCISES,
+	MENTORING_TEACHING
+}
---
+++ src/app/model/type-of-topic.ts
@@ -0,0 +1,10 @@
+export interface TypeOfTopic {
+    id: number,
+    naziv: TypeTopic;
+}
+
+export enum TypeTopic {
+    SUBJECT,
+    EVALUATION_OF_KNOWLEDGE,
+    LESSON_TERM
+}
